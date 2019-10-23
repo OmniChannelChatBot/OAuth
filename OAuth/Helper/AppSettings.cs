0d57a5e539ec976a9af6e6498e2b4f123de19702
Дмитрий Николаевич Вознесенский <kantuzzz@live.com>
2019-10-23 22:21:08 +0300

    UserController

---
+++ OAuth/Helper/AppSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace OAuth.Helper
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # OAuth
+
+https://jasonwatmore.com/post/2019/10/11/aspnet-core-3-jwt-authentication-tutorial-with-example-api
