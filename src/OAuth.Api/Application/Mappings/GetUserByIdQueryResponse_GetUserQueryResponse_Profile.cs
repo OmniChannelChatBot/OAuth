276fd7d84877576a5e64a02a01de6704d4f36a73
ZEXSM <zaxabor@gmail.com>
2020-02-17 21:14:46 +0300

    #ZEXSM#rename fix

--- src/OAuth.Api/Application/Mappings/GetUserByIdQueryResponse_GetUserQueryResponse_Profile.cs
+++ src/OAuth.Api/Application/Mappings/GetUserByIdQueryResponse_GetUserQueryResponse_Profile.cs
@@ -4,9 +4,9 @@ using OAuth.Infrastructure.Services;
 
 namespace OAuth.Api.Application.Mappings
 {
-    public class GetUserByIdQueryResponse_GetByUsernameQueryResponse_Profile : Profile
+    public class GetUserByIdQueryResponse_GetUserQueryResponse_Profile : Profile
     {
-        public GetUserByIdQueryResponse_GetByUsernameQueryResponse_Profile()
+        public GetUserByIdQueryResponse_GetUserQueryResponse_Profile()
         {
             CreateMap<GetUserByIdQueryResponse, GetUserQueryResponse>()
                 .ForMember(d => d.UserId, o => o.MapFrom(m => m.Id));
